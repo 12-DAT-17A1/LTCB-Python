dbefb106d27f6f5fcb66b2348353f5555497a1ca
Nguyen Tien Dat <nguyendat062506@gmail.com>
2024-03-17 14:23:55 +0700

    update

---
+++ Lab1/Bai4.py
@@ -0,0 +1,13 @@
+# Nhập tọa độ vectơ a
+a_x = float(input("Nhập tọa độ x của vectơ a: "))
+a_y = float(input("Nhập tọa độ y của vectơ a: "))
+
+# Nhập tọa độ vectơ b
+b_x = float(input("Nhập tọa độ x của vectơ b: "))
+b_y = float(input("Nhập tọa độ y của vectơ b: "))
+
+# Tính tích vô hướng
+tich_vo_huong = a_x * b_x + a_y * b_y
+
+# In kết quả
+print("Tích vô hướng của 2 vectơ a và b là:", tich_vo_huong)
