237b175fccb3f4ec32ee24bedd92b4d527bd774c
Nguyen Tien Dat <nguyendat062506@gmail.com>
2024-04-10 22:01:54 +0700

    update

--- Lab11/Bai6/Bai6.py
+++ Lab11/Bai6/Bai6.py
@@ -1,11 +1,13 @@
 #a
+print("Hiển thị nội dung dòng đầu tiên và dòng  thứ ba  là:")
 with open("./matrix.txt",mode="r",encoding="utf-8") as file_matrix:
-       print(file_matrix.readline())
+       print(file_matrix.readline().strip().replace("\n",""))
        file_matrix.readline()
-       print(file_matrix.readline())
+       print(file_matrix.readline().strip().replace("\n",""))
 #b
 with open("./matrix.txt",mode="r",encoding="utf-8") as file_matrix:
        list_chain =[" ".join(i.strip().split()) for i in file_matrix.readlines()]
+print("Hiện thị nội dụng toàn bộ file matrix.txt:")
 for chr in list_chain:
        print(chr)
 #c
@@ -26,3 +28,10 @@ for i in create_matrix():
 list_matrix_new_chain = [" ".join(map(str,i))+"\n" for i in list_matrix_new]
 with open("./ODD.txt",mode="w") as file_ood:
        file_ood.writelines(list_matrix_new_chain)
+#d
+with open("./ODD.txt", mode="r",encoding="utf-8") as file_ood:
+       file_ood.readline()
+       file_ood.readline()
+       file_ood.readline()
+       print("Dòng cuối của OOD.txt là:")
+       print(file_ood.readline().strip().replace("\n",""))
